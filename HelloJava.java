580ae18409934d15cc136d5e59d07b74505ea33a
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-04-09 00:27:42 +0200

    initial commit

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+package com.jenkinsCI;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// Prints "Hello, World with Jenkins" in the terminal window
+		System.out.println("Hello, World with Jenkins");
+
+	}
+
+}
